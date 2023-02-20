2d6f6295f092417e0b0f78d67e95ba66b03c7656
vymo2013 <70943745+vymo2013@users.noreply.github.com>
2023-02-20 22:55:32 +0530

    Meta Update

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ import SEO from '../components/SEO';
 function ErrorPage() {
   return (
     <Layout>
-      <SEO title="404" />
+      <SEO title="404 Page" description="The page you are looking for does not exist. It may have been moved, or removed altogether. Perhaps you can return back to the site’s homepage and see if you can find what you are looking for." />
       <HeroSection />
       <GetStartedSection />
     </Layout>
