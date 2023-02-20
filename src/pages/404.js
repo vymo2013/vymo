214e37132690b4e51f65b1ce552af787cf4552d2
vymo2013 <70943745+vymo2013@users.noreply.github.com>
2023-02-20 22:56:36 +0530

    404 update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import HeroSection from '../components/pages/Error/HeroSection';
-import GetStartedSection from '../components/sections/GetStartedSection/GetStartedSection';
 import SEO from '../components/SEO';
 
 function ErrorPage() {
@@ -9,7 +8,6 @@ function ErrorPage() {
     <Layout>
       <SEO title="404 Page" description="The page you are looking for does not exist. It may have been moved, or removed altogether. Perhaps you can return back to the site’s homepage and see if you can find what you are looking for." />
       <HeroSection />
-      <GetStartedSection />
     </Layout>
   );
 }
