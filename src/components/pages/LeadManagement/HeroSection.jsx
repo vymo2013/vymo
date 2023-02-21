6c8f10af3b45791df77eff1d5f9413cc4c171c13
vymo2013 <70943745+vymo2013@users.noreply.github.com>
2023-02-21 15:06:10 +0530

    Lead Management Page update

--- src/components/pages/LeadManagement/HeroSection.jsx
+++ src/components/pages/LeadManagement/HeroSection.jsx
@@ -4,33 +4,32 @@ import SectionHeading from '../../Heading/SectionHeading';
 function HeroSection() {
   return (
     <section className="relative section-pad hero-section overflow-hidden">
-      <div className="container mt-5 xl:mt-0">
+      <div className="container mt-5 xl:mt-1">
         <div className="flex flex-wrap justify-between items-center px-[15px] xl:px-0">
-          <div className="w-full xl:w-[47%] text-center xl:text-left xl:-mt-3">
-            <SectionHeading className="heading">
-              Don’t let a single lead slip out of your funnel unaccounted.
-            </SectionHeading>
-            <p className="text-darkPurple font-medium opacity-60 leading-5 md:leading-7 mt-1">
-              Take control of your entire lead lifecycle with Vymo’s Lead
-              Management System.{' '}
-              <span className="block mt-2">
+          <div className="w-full xl:w-[55%] text-center xl:text-left xl:-mt-3 lg:pt-5">
+            <h1
+              className="text-left mx-auto text-56 leading-[44px] lg:mt-1 font-iight md:leading-[68px]">
+              <span className="font-bold">Convert</span> <i>more leads to customers,</i> <span className="font-bold">predictably</span>.
+            </h1>
+            <p className="text-darkPurple font-medium opacity-60 leading-5 md:leading-7 mt-2">
+              Take control of your entire lead lifecycle with Vymo’s Lead Management System. wSet up complex Lead Workflows for different types of leads and channels, gamify your Sales Playbook, and integrate your sales funnel so your lead progresses through your funnel hassle-free.
+              {/*<span className="block mt-2">
                 Set up complex Lead Workflows for different types of leads and
                 channels, gamify your Sales Playbook, and integrate your sales
                 funnel so your lead progresses through your funnel hassle-free.
-              </span>
+              </span>*/}
             </p>
-            <Button className="mt-1 md:mt-2 started-btn" large>
+            <Button className="mt-1 md:mt-2 red-btn">
               See how we can help
             </Button>
           </div>
-          <div className="w-full xl:w-[50%] select-none mt-[40px] xl:mt-0">
-            <div className="xl:h-[577px] xl:w-[829px] relative">
+          <div className="w-full xl:w-[45%] select-none mt-[40px] xl:mt-0">
+            <div className="xl:w-[800px] relative md:pl-2">
               <img
                 src="/img/lead-management/hero-bg15.png"
-                width="1243"
-                height="866"
-                alt=""
-                className="max-w-[120%] xl:max-w-[110%]"
+                width="100%"
+                alt="lead management banner"
+
               />
             </div>
           </div>
